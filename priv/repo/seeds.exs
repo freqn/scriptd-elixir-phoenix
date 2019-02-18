465c86be8411daf939a1833d37dfdece5159da3b
freqn <freqn@freqns-MacBook-Pro-2.local>
2019-02-18 00:23:41 -0500

    add seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,19 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Scriptd.Repo.insert!(%Scriptd.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Scriptd.Repo
+alias Scriptd.Accounts
+alias Accounts.Location
+alias Accounts.Patient
+alias Accounts.Pharmacy
+alias Accounts.Prescription
+
+
+Repo.insert!(%Pharmacy{name: "Alfa Pharmacy"})
+Repo.insert!(%Pharmacy{name: "Bravo Pharmacy"})
+
+Repo.insert!(%Location{latitude: "39.9612", longitude: "82.9988"})
+Repo.insert!(%Location{latitude: "40.9612", longitude: "72.9988"})
+
+Repo.insert!(%Prescription{name: "Allegra"})
+Repo.insert!(%Prescription{name: "Rolaids"})
+
+Repo.insert!(%Patient{first_name: "First", last_name: "User"})
+Repo.insert!(%Patient{first_name: "Second", last_name: "User"})
